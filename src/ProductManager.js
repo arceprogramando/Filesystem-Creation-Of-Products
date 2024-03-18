94ee861fe31295c1e0ca22d70ef84432d493e810
Felipe Arce <arceprogramando@gmail.com>
2024-03-18 02:17:17 -0300

    refactor(backend): :recycle: Remove unnecessary endpoints and refactor the code for improved efficiency.

--- src/ProductManager.js
+++ src/ProductManager.js
@@ -1,34 +1,30 @@
-import { promises as fs } from 'fs';
+import fs from 'fs/promises';
 
 class ProductManager {
+  constructor(filePath) {
+    this.filePath = filePath;
+  }
 
-    constructor(filePath) {
-        this.filePath = filePath;
+  async getProducts() {
+    try {
+      const data = await fs.readFile(this.filePath, 'utf-8');
+      const products = JSON.parse(data);
+      return products;
+    } catch (error) {
+      throw new Error('Error al obtener los productos ');
     }
+  }
 
-    async getProducts() {
-
-        try {
-            const data = await fs.readFile(this.filePath, 'utf-8');
-            const products = JSON.parse(data);
-            return products;
-        } catch (error) {
-            throw new Error('Error al obtener los productos ')
-        }
+  async getProductById(pid) {
+    try {
+      const data = await fs.readFile(this.filePath, 'utf-8');
+      const products = JSON.parse(data);
+      const product = products.find((p) => p.id === Number(pid));
+      return product;
+    } catch (error) {
+      throw new Error('Error al obtener los productos.');
     }
-
-    async getProductById(pid) {
-
-        try {
-            const data = await fs.readFile(this.filePath, 'utf-8');
-            const products = JSON.parse(data);
-            const product = products.find((p) => p.id === parseInt(pid));
-            return product;
-        }
-        catch (error) {
-            throw new Error('Error al obtener los productos.')
-        }
-    }
+  }
 }
 
 export default ProductManager;
--- src/app.js
+++ src/app.js
@@ -1,51 +1,39 @@
-import express, { json, urlencoded } from 'express';
+import express from 'express';
 import ProductManager from './ProductManager.js';
 
 const app = express();
 
-app.use(json())
-app.use(urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+const PORT = 8080;
+
+app.listen(PORT, () => {
+  console.log(`Escuchando en puerto: http://localhost:${PORT}`);
+});
 
 const productManager = new ProductManager('./files/products.json');
 
-app.get('/', (req, response) => {
-    response.status(200).send('Todo salio bien')
-})
-
-app.get('/products', async (req, res) => {
-    try {
-        const limit = req.query.limit;
-        const products = await productManager.getProducts();
-        console.log(products)
-        if (limit) {
-            const limitedProducts = products.slice(0, limit);
-            res.json(limitedProducts);
-        } else {
-            res.json(products)
-        }
-    }
-    catch (error) {
-        res.status(500).json({ error: 'Error al obtener los productos' });
-    }
-})
-
-app.get('/products/:pid', async (req, res) => {
-    try {
-        const pid = req.params.pid;
-        const product = await productManager.getProductById(pid);
-
-        if (product) {
-            res.json(product);
-        } else {
-            res.status(404).json({ error: 'El producto no existe' });
-        }
-
-    } catch (error) {
-        res.status(500).json({ error: `Error al obtener el producto con el id solicitado` });
-    }
+app.get('/', async (req, res) => {
+  try {
+    const { limit } = req.query;
+    const products = await productManager.getProducts();
+    if (limit) return res.json(products.slice(0, limit));
+    return res.json(products);
+  } catch (error) {
+    return res.status(404).json({ error: error.message });
+  }
 });
 
+app.get('/:pid', async (req, res) => {
+  try {
+    const { pid } = req.params;
+    const product = await productManager.getProductById(pid);
 
-const PORT = 8080;
-app.listen(PORT, () => { console.log(`Listening on port 8080\nhttp://localhost:${PORT}`) }) 
+    if (product) return res.json(product);
+
+    return res.status(404).json({ error: 'El producto no existe' });
+  } catch (error) {
+    return res.status(500).json({ error: 'Error al obtener el producto con el id solicitado' });
+  }
+});
